c3658a024914f7ea39a2988aad87c79bb8fab5e3
fanyingmao <646900491@qq.com>
2019-09-02 23:45:42 +0800

    layout

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './index.css';
+import { AutoComplete, Icon, Input } from 'antd';
+function onSelect(value: string) {
+  console.log('onSelect', value);
+}
 
 const BasicLayout: React.FC = props => {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
+    <div className={styles.base}>
+      <div className={styles.normal}>
+        <h1 className={styles.title}>Minimalist Admin</h1>
+        <AutoComplete />
+      </div>
+      <div className={styles.children}>
+        {props.children}
+      </div>
     </div>
   );
 };
