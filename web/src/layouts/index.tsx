e41c674172d44b5178fc3e9340f48a0836e2c54e
fanyingmao <646900491@qq.com>
2019-09-17 20:28:17 +0800

    界面修改

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './index.css';
-import { AutoComplete, Icon, Input } from 'antd';
+import { AutoComplete, Icon, Input, Button } from 'antd';
 function onSelect(value: string) {
   console.log('onSelect', value);
 }
@@ -8,13 +8,7 @@ function onSelect(value: string) {
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.base}>
-      <div className={styles.normal}>
-        <h1 className={styles.title}>Minimalist Admin</h1>
-        <AutoComplete />
-      </div>
-      <div className={styles.children}>
         {props.children}
-      </div>
     </div>
   );
 };
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
-import styles from './index.css';
+  
+import { Alert, Checkbox, Icon, Button, AutoComplete } from 'antd';
+import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
+import React, { Component } from 'react';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { Dispatch, AnyAction } from 'redux';
+import { FormComponentProps } from 'antd/es/form';
+import Link from 'umi/link';
+import { connect } from 'dva';
+
+
+interface LoginProps {
+  dispatch: Dispatch<AnyAction>;
+  submitting: boolean;
 }
+interface LoginState {
+  type: string;
+  autoLogin: boolean;
+}
+
+
+class Admin extends Component<LoginProps, LoginState> {
+  loginForm: FormComponentProps['form'] | undefined | null = undefined;
+
+  state: LoginState = {
+    type: 'account',
+    autoLogin: true,
+  };
+
+  changeAutoLogin = (e: CheckboxChangeEvent) => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
+  };
+
+
+  onTabChange = (type: string) => {
+    this.setState({ type });
+  };
+
+
+  renderMessage = (content: string) => (
+    <Alert style={{ marginBottom: 24 }} message={content} type="error" showIcon />
+  );
+
+  render() {
+    const { submitting } = this.props;
+    const { type, autoLogin } = this.state;
+    return (
+      <div >
+          <Button type="primary">fdsafdasf</Button>
+          <AutoComplete />
+      </div>
+    );
+  }
+}
+
+export default Admin;
