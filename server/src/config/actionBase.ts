08e12df2049137734ce3a2789e53e81b3f9749a9
fanyingmao <you@example.com>
2019-09-24 14:04:20 +0800

    配置修改

--- server/src/config/actionBase.ts
+++ server/src/config/actionBase.ts
@@ -1,16 +1,22 @@
+/**
+ * 这里需要注意数据库密码，服务器ip域名不能提交到github
+ * 这里进行命令类型的拼接，如mysql redis 将xxxx 换为自己服务器的配置
+ */
+
+ const BasePrefix = `ssh root@xxxxxxx`;//这里填要远程执行的ssh如果是本机的话为空
 export const ActionType = {
   system: {
     type: 0,
-    // Prefix: "ssh root@by.fanym.top '<subCmd>'"
-    Prefix: "<subCmd>"
+    Prefix: `${BasePrefix} \"<subCmd>\"`
+    // Prefix: "<subCmd>"
   },
   mysql: {
     type: 1,
-    Prefix: "<subCmd>"
+    Prefix: `${BasePrefix} \' mysql -h xxxxxxx -uroot -pxxxxxxx --database=xxxxxxx -e \"<subCmd>\"\'` //这里根据自己数据库进行配置
   },
   redis: {
     type: 2,
-    Prefix: "<subCmd>"
+    Prefix: `redis-cli <subCmd>`
   }
 }
  let temMap:any = {};
